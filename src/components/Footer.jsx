32029007282cb9455528e1e0d2b31e0e2c921794
Fillesa <fillesa.mehaj@gmail.com>
2022-02-12 22:03:35 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <div className='text-center p-10 mt-10 border-t dark:border-grey-700 border-gray-300'>
+            <h1>2022 Goggl, Inc</h1>
+        </div>
+    )
+}
+
+export default Footer;
